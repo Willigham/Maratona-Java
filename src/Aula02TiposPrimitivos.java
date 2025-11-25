62cb83e7b1cc6854d76f10fca1b5d004d1887fba
Willigham Coelho <willigamcoelho@gmail.com>
2025-11-25 01:15:41 -0300

    Commit 25.11 - Aula 02

---
+++ src/Aula02TiposPrimitivos.java
@@ -0,0 +1,18 @@
+public class Aula02TiposPrimitivos {
+    public static void main (String [] args){
+        // int, double, float, char, byte, short, long, boolean
+        int idade = 10;
+        long numeroLong = 100000;
+        double salarioDouble = 2000.0;
+        float salarioFloeat = 2500.0F;
+        byte idadeByte = 127;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041';
+
+        System.out.println("A idade é " + idade + " anos");
+        System.out.println(falso);
+        System.out.println("char " + caractere);
+    }
+}
