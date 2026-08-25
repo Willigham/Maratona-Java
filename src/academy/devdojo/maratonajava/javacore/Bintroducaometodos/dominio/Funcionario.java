12848455fac8cb885558c1fb188547e26f8e2b4e
Willigham Coelho <willigamcoelho@gmail.com>
2026-08-25 20:14:16 -0300

    Commit 25.08.2026 - POO / Modificador de Acesso Pt 02

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
@@ -30,4 +30,28 @@ public class Funcionario {
         media /= salarios.length;
         System.out.println("Média salarial: " + media);
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double[] getSalarios() {
+        return salarios;
+    }
+
+    public void setSalarios(double[] salarios) {
+        this.salarios = salarios;
+    }
 }
--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest06.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest06.java
@@ -6,9 +6,9 @@ public class CalculadoraTest06 {
     public static void main(String[] args) {
         Funcionario funcionario01 = new Funcionario();
 
-        funcionario01.nome = "Pedro";
-        funcionario01.idade = 35;
-        funcionario01.salarios = new double[]{1200, 987.32, 2000};
+        funcionario01.setNome("Sanji")
+        funcionario01.setIdade(35);
+        funcionario01.setSalarios(new double[]{1200, 987.32, 2000});
         funcionario01.imprimirDados();
     }
 
