c13090e3c0ff6d996ff70fec81df6f91f73efdeb
Willigham Coelho <willigamcoelho@gmail.com>
2026-08-25 20:58:13 -0300

    Commit 25.08.2026 - POO / Sobrecarga de Metodos

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest06.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest06.java
@@ -6,7 +6,7 @@ public class CalculadoraTest06 {
     public static void main(String[] args) {
         Funcionario funcionario01 = new Funcionario();
 
-        funcionario01.setNome("Sanji")
+        funcionario01.setNome("Sanji");
         funcionario01.setIdade(35);
         funcionario01.setSalarios(new double[]{1200, 987.32, 2000});
         funcionario01.imprimirDados();
