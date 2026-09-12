614c05fa3783cbff9a547c6da0450483c697c729
Willigham Coelho <willigamcoelho@gmail.com>
2026-09-11 23:21:03 -0300

    Commit 11.09.2026 - POO / Heranca Super

--- src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
@@ -4,6 +4,12 @@ public class Funcionario extends Pessoa {
 
     private double salario;
 
+    @Override
+    public void imprime() {
+        super.imprime();
+        System.out.println(this.salario);
+    }
+
     public double getSalario() {
         return salario;
     }
