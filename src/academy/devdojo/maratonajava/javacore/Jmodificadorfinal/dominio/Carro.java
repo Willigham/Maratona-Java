89d90cb76e1033bebfe18c98a81a0667df72d4b3
Willigham Coelho <willigamcoelho@gmail.com>
2026-09-12 00:28:01 -0300

    Commit 12.09.2026 - POO / modificaor Final

---
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Carro.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio;
+
+public class Carro {
+    private String nome;
+    public final static double VELOCIDADE_lIMITE = 250;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
